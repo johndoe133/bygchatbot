42050f008041565703b159cb99f27e1d75f8866d
eric <erickastljensen@gmail.com>
2020-04-01 12:57:32 +0200

    not working user friendly

--- test.py
+++ test.py
@@ -1,12 +1,95 @@
-import json
+from chatbot import (token, get, getBuildingFloorArea, getBuildingHeight, 
+                    getBuildingVolume, getJson, getHeight, getVolume, getFloorArea)
+import logging
 
-with open('beats.json') as json_file:
-    json_obj = json.load(json_file)
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
 
-json_formatted_str = json.dumps(json_obj, indent=2)
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
-# print(json_obj[0]['segments'][0]['id'])
+logger = logging.getLogger(__name__)
 
-# print(json_formatted_str)
+TEAM, INFO = range(2)
 
-# print(json_obj[0]['name'])
+
+def start(update, context):
+    reply_keyboard = [[]]
+    json_obj = getJson('beats.json')
+    for team in json_obj:
+        reply_keyboard[0] += team['team']
+
+    update.message.reply_text(
+        'Hi! I am the BygBot. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Which team would you like to examine?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return TEAM
+
+
+def info(update, context):
+    reply_keyboard = [['Height','Volume','Floor Area']]
+    json_obj = getJson('beats.json')
+    update.message.reply_text(
+        'What information would you like?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return TEAM
+    return ConversationHandler.END
+
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(token, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            TEAM: [MessageHandler(Filters.regex(''), start)],
+
+            INFO: [MessageHandler(Filters.regex(''), info)],
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
